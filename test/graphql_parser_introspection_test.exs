37d67ee588291e7da6982ce8380ac3b439b84037
Josh Price <josh@canoniq.com>
2015-09-19 12:21:33 +1000

    Introspection Query
    Won't parse until #6 is fixed

---
+++ test/graphql_parser_introspection_test.exs
@@ -0,0 +1,88 @@
+defmodule GraphqlParserIntrospectionTest do
+  use ExUnit.Case, async: true
+
+  def assert_parse(input_string, expected_output) do
+    assert GraphQL.parse(input_string) == expected_output
+  end
+
+  test "Introspection Query" do
+    assert_parse """
+      # The introspection query to end all introspection queries, copied from
+      # https://github.com/graphql/graphql-js/blob/master/src/utilities/introspectionQuery.js
+
+      query IntrospectionQuery {
+        __schema {
+          queryType { name }
+          mutationType { name }
+          types {
+            ...FullType
+          }
+          directives {
+            name
+            description
+            args {
+              ...InputValue
+            }
+            onOperation
+            onFragment
+            onField
+          }
+        }
+      }
+      fragment FullType on __Type {
+        kind
+        name
+        description
+        fields {
+          name
+          description
+          args {
+            ...InputValue
+          }
+          type {
+            ...TypeRef
+          }
+          isDeprecated
+          deprecationReason
+        }
+        inputFields {
+          ...InputValue
+        }
+        interfaces {
+          ...TypeRef
+        }
+        enumValues {
+          name
+          description
+          isDeprecated
+          deprecationReason
+        }
+        possibleTypes {
+          ...TypeRef
+        }
+      }
+      fragment InputValue on __InputValue {
+        name
+        description
+        type { ...TypeRef }
+        defaultValue
+      }
+      fragment TypeRef on __Type {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+            }
+          }
+        }
+      }
+    """, []
+  end
+end
