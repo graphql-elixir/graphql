d97316225c1ccb080c45e77ffbba507909e3ffcb
Josh Price <josh@canoniq.com>
2015-10-08 11:09:58 +1100

    Switch contributors to maintainers because deprecation

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule GraphQL.Mixfile do
   defp package do
     [# These are the default files included in the package
      files: ["lib", "src/*.xrl", "src/*.yrl", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
-     contributors: ["Josh Price", "James Sadler"],
+     maintainers: ["Josh Price", "James Sadler"],
      licenses: ["BSD"],
      links: %{"GitHub" => "https://github.com/joshprice/graphql-elixir"}]
   end
