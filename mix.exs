df7b0039fdfa8cb8fae7cffa310122222d3175a0
Josh Price <josh@canoniq.com>
2015-10-08 11:11:29 +1100

    Remove hex warnings

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule GraphQL.Mixfile do
 
   defp package do
     [# These are the default files included in the package
-     files: ["lib", "src/*.xrl", "src/*.yrl", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     files: ["lib", "src/*.xrl", "src/*.yrl", "mix.exs", "README*", "LICENSE*"],
      maintainers: ["Josh Price", "James Sadler"],
      licenses: ["BSD"],
      links: %{"GitHub" => "https://github.com/joshprice/graphql-elixir"}]
