66145c6a022a1ebacc9a4ffd122a83f369ce455a
Josh Price <josh@canoniq.com>
2015-11-20 10:27:33 +1100

    Publish 0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GraphQL.Mixfile do
   def project do
     [app: :graphql,
      name: "GraphQL",
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      description: description,
      package: package,
