7f8c708fc5611fe0aee03706309909b89b37d494
Nick Gartmann <nick.gartmann@gmail.com>
2015-11-24 16:35:01 -0600

    Added support for object fields
    Allows queries to select for fields inside of
    object type fields.
    
    Ex:
      {
        greeting
        person {
          name
          title
        }
      }
    
      will yeild
    
      {
        "greeting: "Hello world!",
        "person": {
            "name": "Nick Gartmann",
            "title": "Mr."
          }
      }
    
    Currently relying on placing
    a GraphQL.ObjectType in the field list.

--- lib/graphql/exceptions.ex
+++ lib/graphql/exceptions.ex
@@ -8,3 +8,15 @@ defmodule GraphQL.SyntaxError do
     "#{exception.errors} on line #{exception.line}"
   end
 end
+
+defmodule GraphQL.QueryError do 
+  @moduledoc """
+  An error raised when a field is queried that does not exist
+  """
+
+  defexception line: nil, errors: "Query error", param: nil
+  def message(exception) do
+    ""
+  end
+end
+
