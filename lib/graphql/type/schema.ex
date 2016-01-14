f51070e03d1e37b10542133e941cdec11fa44ed1
Mark Olson <theothermarkolson@gmail.com>
2016-01-14 13:53:24 -0800

    Tiny cleanup for Schema.reduce_types

--- lib/graphql/type/schema.ex
+++ lib/graphql/type/schema.ex
@@ -2,36 +2,28 @@ defmodule GraphQL.Schema do
   defstruct query: nil, mutation: nil, types: []
 
   def reduce_types(type) do
-    o = reduce_types(type.query, %{})
-    reduce_types(GraphQL.Type.Introspection.schema, o)
+    %{}
+    |> reduce_types(type.query)
+    |> reduce_types(GraphQL.Type.Introspection.schema)
   end
 
-  def reduce_types(%GraphQL.Type.List{of_type: list_type}, typemap), do: reduce_types(list_type, typemap)
-  def reduce_types(%GraphQL.Type.NonNull{of_type: list_type}, typemap), do: reduce_types(list_type, typemap)
+  def reduce_types(typemap, %GraphQL.Type.List{of_type: list_type}), do: reduce_types(typemap, list_type)
+  def reduce_types(typemap, %GraphQL.Type.NonNull{of_type: list_type}), do: reduce_types(typemap, list_type)
 
-  def reduce_types(%GraphQL.Type.Interface{} = type, typemap) do
+  def reduce_types(typemap, %GraphQL.Type.Interface{} = type) do
     Map.put(typemap, type.name, type)
   end
 
-  def reduce_types(%GraphQL.Type.Enum{} = type, typemap) do
-    Map.put(typemap, type.name, type)
-  end
-  def reduce_types(%GraphQL.Type.Boolean{} = type, typemap), do: Map.put(typemap, type.name, type)
-  def reduce_types(%GraphQL.Type.ID{} = type, typemap), do: Map.put(typemap, type.name, type)
-  def reduce_types(%GraphQL.Type.String{} = type, typemap), do: Map.put(typemap, type.name, type)
-
-  def reduce_types(type, typemap) do
+  def reduce_types(typemap, %GraphQL.Type.ObjectType{} = type) do
     if Map.has_key?(typemap, type.name) do
       typemap
     else
       typemap = Map.put(typemap, type.name, type)
-      case type do
-        %GraphQL.Type.ObjectType{} ->
-          thunk_fields = GraphQL.Execution.Executor.maybe_unwrap(type.fields)
-          Enum.reduce(thunk_fields, typemap, fn({_,fieldtype},map) ->
-            reduce_types(fieldtype.type, map)
-          end)
-      end
+      thunk_fields = GraphQL.Execution.Executor.maybe_unwrap(type.fields)
+      Enum.reduce(thunk_fields, typemap, fn({_,fieldtype},map) ->
+        reduce_types(map, fieldtype.type)
+      end)
     end
   end
+  def reduce_types(typemap, %{name: name} = type), do: Map.put(typemap, name, type)
 end
