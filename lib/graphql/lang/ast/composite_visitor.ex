9b0a97d531bf7c0a08ec7fe4b4f84d1ef904cb36
James Sadler <freshtonic@gmail.com>
2016-03-11 16:27:04 +0700

    Provide means to combine multiple Visitors into one
    This will allow us to write a type information visitor that 'wraps' a
    validation visitor so that when *enter* and *leave* are called on the
    validation visitor, type information for the current node will be
    available on the accumulator.
    
    It also makes it easy to insert visitors in a chain and debug what's
    going on when writing tests.

---
+++ lib/graphql/lang/ast/composite_visitor.ex
@@ -0,0 +1,108 @@
+
+alias GraphQL.Lang.AST.Visitor
+alias GraphQL.Lang.AST.InitialisingVisitor
+alias GraphQL.Lang.AST.PostprocessingVisitor
+
+defmodule GraphQL.Lang.AST.CompositeVisitor do
+  @moduledoc """
+  A CompositeVisitor composes two Visitor implementations into a single Visitor.
+  """
+
+  defstruct outer_visitor: nil, inner_visitor: nil
+
+  @doc """
+  Composes two Visitors, returning a new one.
+  """
+  def compose(outer_visitor, inner_visitor) do
+    %GraphQL.Lang.AST.CompositeVisitor{ outer_visitor: outer_visitor, inner_visitor: inner_visitor }
+  end
+
+  @doc """
+  Composes an arbitrarily long list of Visitors into a single Visitor.
+
+  The order of the list is outer-to-inner.  The leftmost visitor will be invoked first
+  upon 'enter' and last upon 'leave'.
+  """
+  def compose(visitors) when is_list(visitors) and length(visitors) >= 2 do
+    [outer_visitor|[inner_visitor|rest]] = visitors 
+    composite_visitor = compose(outer_visitor, inner_visitor)
+    if length(rest) > 0 do
+      compose([composite_visitor] ++ rest)
+    else
+      composite_visitor
+    end
+  end
+end
+
+defimpl Visitor, for: GraphQL.Lang.AST.CompositeVisitor do
+
+  @doc """
+  Invoke *enter* on the outer visitor first, passing the resulting accumulator to the *enter*
+  call on the *inner* visitor.
+
+  If either visitor's enter method returns :break, both visitors will still be executed, but
+  then execution will cease.
+  """
+  def enter(composite_visitor, node, accumulator) do
+    call_in_order(
+      composite_visitor.outer_visitor,
+      composite_visitor.inner_visitor,
+      node, accumulator, &Visitor.enter/3)
+  end
+
+  @doc """
+  Invoke *leave* on the inner visitor first, passing the resulting accumulator to the *leave*
+  call on the *outer* visitor.
+
+  If either visitor's enter method returns :break, both visitors will still be executed, but
+  then execution will cease.
+  """
+  def leave(composite_visitor, node, accumulator) do
+    call_in_order(
+      composite_visitor.inner_visitor,
+      composite_visitor.outer_visitor,
+      node, accumulator, &Visitor.leave/3)
+  end
+
+  # Visits two visitors in the order specified and invokes the supplied Visitor function.
+  defp call_in_order(v1, v2, node, accumulator, fun) do
+    { v1_next_action, v1_accumulator } = fun.(v1, node, accumulator)
+    accumulator = Map.merge(accumulator, v1_accumulator)
+    { v2_next_action, v2_accumulator } = fun.(v2, node, accumulator)
+
+    next_action = if v1_next_action == :break do
+      :break
+    else 
+      v2_next_action
+    end
+    
+    { next_action, v2_accumulator }
+  end
+end
+
+defimpl InitialisingVisitor, for: GraphQL.Lang.AST.CompositeVisitor do
+  @doc """
+  Invokes *start* on the outer visitor first, then calls *start* on the inner visitor
+  passing the accumulator from the *outer*.
+
+  Returns the accumulator of the *inner* visitor.
+  """
+  def init(composite_visitor, accumulator) do
+    accumulator = composite_visitor.outer_visitor |> InitialisingVisitor.init(accumulator)
+    composite_visitor.inner_visitor |> InitialisingVisitor.init(accumulator)
+  end
+end
+
+defimpl PostprocessingVisitor, for: GraphQL.Lang.AST.CompositeVisitor do
+  @doc """
+  Invokes *finish* on the inner visitor first, then calls *finish* on the outer visitor
+  passing the accumulator from the *inner*.
+
+  Returns the accumulator of the *outer* visitor.
+  """
+  def finish(composite_visitor, accumulator) do
+    accumulator = composite_visitor.inner_visitor |> PostprocessingVisitor.finish(accumulator)
+    composite_visitor.outer_visitor |> PostprocessingVisitor.finish(accumulator)
+  end
+end
+
---
+++ test/graphql/lang/ast/composite_visitor_test.exs
@@ -0,0 +1,64 @@
+
+defmodule GraphQL.Lang.AST.CompositeVisitorTest do
+  use ExUnit.Case, async: true
+
+  alias GraphQL.Lang.Parser
+  alias GraphQL.Lang.AST.Reducer
+  alias GraphQL.Lang.AST.Visitor
+  alias GraphQL.Lang.AST.CompositeVisitor
+  alias GraphQL.Lang.AST.PostprocessingVisitor
+
+  defmodule TracingVisitor do
+    defstruct name: nil
+  end
+
+  defimpl Visitor, for: TracingVisitor do
+    def enter(visitor, node, accumulator) do
+      { :continue, %{ accumulator | calls: ["#{visitor.name} entering #{node[:kind]}"] ++ accumulator[:calls] } }
+    end
+
+    def leave(visitor, node, accumulator) do
+      { :continue, %{ accumulator | calls: ["#{visitor.name} leaving #{node[:kind]}"] ++ accumulator[:calls] } }
+    end
+  end
+
+  defmodule CallReverser do
+    defstruct name: "call reverser"
+  end
+
+  defimpl PostprocessingVisitor, for: CallReverser do
+    def finish(_visitor, accumulator) do
+      Enum.reverse(accumulator[:calls])
+    end
+  end
+  
+  test "Composed Visitors are called in the correct order" do
+    v0 = %CallReverser{}
+    v1 = %TracingVisitor{name: "v1"}
+    v2 = %TracingVisitor{name: "v2"}
+    v3 = %TracingVisitor{name: "v3"}
+    composite_visitor = CompositeVisitor.compose([v0, v1, v2, v3])
+    {:ok, ast } = Parser.parse "type Person { name: String }"
+    calls = Reducer.reduce(ast, composite_visitor, %{calls: []})
+    assert calls == [
+      "v1 entering Document", "v2 entering Document", "v3 entering Document",
+      "v1 entering ObjectTypeDefinition", "v2 entering ObjectTypeDefinition",
+      "v3 entering ObjectTypeDefinition", "v1 entering Name",
+      "v2 entering Name", "v3 entering Name", "v3 leaving Name",
+      "v2 leaving Name", "v1 leaving Name", "v1 entering FieldDefinition",
+      "v2 entering FieldDefinition", "v3 entering FieldDefinition",
+      "v1 entering Name", "v2 entering Name", "v3 entering Name",
+      "v3 leaving Name", "v2 leaving Name", "v1 leaving Name",
+      "v1 entering NamedType", "v2 entering NamedType", "v3 entering NamedType",
+      "v1 entering Name", "v2 entering Name", "v3 entering Name",
+      "v3 leaving Name", "v2 leaving Name", "v1 leaving Name",
+      "v3 leaving NamedType", "v2 leaving NamedType", "v1 leaving NamedType",
+      "v3 leaving FieldDefinition", "v2 leaving FieldDefinition",
+      "v1 leaving FieldDefinition", "v3 leaving ObjectTypeDefinition",
+      "v2 leaving ObjectTypeDefinition", "v1 leaving ObjectTypeDefinition",
+      "v3 leaving Document", "v2 leaving Document", "v1 leaving Document"
+    ]
+  end
+end
+
+
