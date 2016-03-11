05025a739054f1b24ab0cc527de6fb37241b0019
James Sadler <freshtonic@gmail.com>
2016-03-11 16:23:37 +0700

    Implement the AST Reducer
    The AST Reducer walks the AST of a GraphQL query and invokes a Visitor
    as nodes of the AST are traversed. The Visitor is responsible for
    maintaining an accumulator.
    
    At the end of the traversal, the final values in the accumulator can be
    obtained. This lays the groundwork for implementing query validators
    (the majority, if not all, will be Visitor implementations).

---
+++ lib/graphql/lang/ast/nodes.ex
@@ -0,0 +1,41 @@
+
+defmodule GraphQL.Lang.AST.Nodes do
+  @kinds %{
+    Name:                       [],
+    Document:                   [:definitions],
+    OperationDefinition:        [:name, :variableDefinitions, :directives, :selectionSet],
+    VariableDefinition:         [:variable, :type, :defaultValue],
+    Variable:                   [:name],
+    SelectionSet:               [:selections],
+    Field:                      [:alias, :name, :arguments, :directives, :selectionSet],
+    Argument:                   [:name, :value],
+    FragmentSpread:             [:name, :directives],
+    InlineFragment:             [:typeCondition, :directives, :selectionSet],
+    FragmentDefinition:         [:name, :typeCondition, :directives, :selectionSet],
+    IntValue:                   [],
+    FloatValue:                 [],
+    StringValue:                [],
+    BooleanValue:               [],
+    EnumValue:                  [],
+    ListValue:                  [:values],
+    ObjectValue:                [:fields],
+    ObjectField:                [:name, :value],
+    Directive:                  [:name, :arguments],
+    NamedType:                  [:name],
+    ListType:                   [:type],
+    NonNullType:                [:type],
+    ObjectTypeDefinition:       [:name, :interfaces, :fields],
+    FieldDefinition:            [:name, :arguments, :type],
+    InputValueDefinition:       [:name, :type, :defaultValue],
+    InterfaceTypeDefinition:    [:name, :fields],
+    UnionTypeDefinition:        [:name, :types],
+    ScalarTypeDefinition:       [:name],
+    EnumTypeDefinition:         [:name, :values],
+    EnumValueDefinition:        [:name],
+    InputObjectTypeDefinition:  [:name, :fields],
+    TypeExtensionDefinition:    [:definition]
+  }
+
+  def kinds, do: @kinds
+end
+
---
+++ lib/graphql/lang/ast/visitor.ex
@@ -0,0 +1,92 @@
+
+defmodule GraphQL.Lang.AST do
+  defprotocol Visitor do
+    @moduledoc """
+    Implementations of Visitor are used by the ASTReducer to transform a GraphQL AST
+    into an arbitrary value.
+
+    The value can be the result of validations, or a transformation of the AST into
+    a new AST, for example.
+
+    The fallback implementations of 'enter' and 'leave' return the accumulator untouched.
+    """
+
+    @fallback_to_any true
+
+    @doc """
+    Called when entering a node of the AST.
+
+    The return value should be:
+
+    {next_action, acc}
+
+    where next_action is either :break or :continue and acc is the new value of the accumulator.
+
+    :break will abort the visitor and AST traversal will cease returning the current value of the accumulator.
+    """
+    def enter(visitor, node, accumulator)
+
+    @doc """
+    Called when leaving a node of the AST.
+
+    The return value should be:
+
+    {next_action, acc}
+
+    where next_action is either :break or :continue and acc is the new value of the accumulator.
+
+    :break will abort the visitor and AST traversal will cease returning the current value of the accumulator.
+    """
+    def leave(visitor, node, accumulator)
+  end
+
+  defimpl Visitor, for: Any do
+    def enter(_visitor, _node, accumulator), do: { :continue, accumulator }
+    def leave(_visitor, _node, accumulator), do: { :continue, accumulator }
+  end
+
+  defprotocol InitialisingVisitor do
+    @moduledoc """
+    A Visitor that implements this protocol will have the opportunity to perform some
+    initialisation and set up the accumulator before AST traversal is started.
+
+    The fallback implementation returns the accumulator untouched.
+    """
+
+    @fallback_to_any true
+
+    @doc """
+    Invoked before traversal begins.  The Visitor can do any once-off accumulator initialisation here.
+    """
+    def init(visitor, accumulator)
+  end
+
+  defprotocol PostprocessingVisitor do
+    @moduledoc """
+    A Visitor that implements this protocol will have the opportunity to transform
+    the accumulator into something more consumer friendly. It can often be the case that
+    the working form of the accumulator is not what should be returned from ASTReducer.reduce/3.
+
+    This also means that the accumulator (always a Map) can be transformed into an
+    arbitrary struct or other Erlang/Elixir type.
+
+    The fallback implementation returns the accumulator untouched.
+    """
+
+    @fallback_to_any true
+
+    @doc """
+    Invoked once after traversal ends.  This can be used to transform the accumulator
+    into an arbitrary value.
+    """
+    def finish(visitor, accumulator)
+  end
+
+  defimpl InitialisingVisitor, for: Any do
+    def init(_visitor, accumulator), do: accumulator
+  end
+
+  defimpl PostprocessingVisitor, for: Any do
+    def finish(_visitor, accumulator), do: accumulator
+  end
+end
