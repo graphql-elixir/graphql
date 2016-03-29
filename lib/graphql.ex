07fddd55ee3080fed3f2bf708c21b27d26f28caa
James Sadler <freshtonic@gmail.com>
2016-03-29 13:10:24 +1100

    Refactor: DRY up execution entry points

--- lib/graphql.ex
+++ lib/graphql.ex
@@ -23,9 +23,25 @@ defmodule GraphQL do
       # {:ok, %{hello: world}}
   """
   def execute(schema, query, root_value \\ %{}, variable_values \\ %{}, operation_name \\ nil) do
+    execute_with_optional_validation(true, schema, query, root_value, variable_values, operation_name)
+  end
+
+  @doc """
+  Execute a query against a schema (without validation)
+
+      # iex> GraphQL.execute(schema, "{ hello }")
+      # {:ok, %{hello: world}}
+  """
+  def execute_without_validation(schema, query, root_value \\ %{}, variable_values \\ %{}, operation_name \\ nil) do
+    execute_with_optional_validation(false, schema, query, root_value, variable_values, operation_name)
+  end
+
+  defp execute_with_optional_validation(should_validate, schema, query, root_value, variable_values, operation_name) do
+    # NOTE: it would be nice if we could compose functions together in a chain (with ot without validation step).
+    # See: http://www.zohaib.me/railway-programming-pattern-in-elixir/
     case GraphQL.Lang.Parser.parse(query) do
       {:ok, document} ->
-        case Validator.validate(schema, document) do
+        case optionally_validate(should_validate, schema, document) do
           :ok ->
             case Executor.execute(schema, document, root_value, variable_values, operation_name) do
               {:ok, response} -> {:ok, %{data: response}}
@@ -39,21 +55,6 @@ defmodule GraphQL do
     end
   end
 
-  @doc """
-  Execute a query against a schema (without validation)
-
-      # iex> GraphQL.execute(schema, "{ hello }")
-      # {:ok, %{hello: world}}
-  """
-  def execute_without_validation(schema, query, root_value \\ %{}, variable_values \\ %{}, operation_name \\ nil) do
-    case GraphQL.Lang.Parser.parse(query) do
-      {:ok, document} ->
-        case Executor.execute(schema, document, root_value, variable_values, operation_name) do
-          {:ok, response} -> {:ok, %{data: response}}
-          {:error, errors} -> {:error, errors}
-        end
-      {:error, errors} ->
-        {:error, errors}
-    end
-  end
+  defp optionally_validate(false, _schema, _document), do: :ok
+  defp optionally_validate(true, schema, document), do: Validator.validate(schema, document)
 end
