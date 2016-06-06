a3939cf4aa4ecd212c2c71d808c471aea0f9612e
Josh Price <josh@canoniq.com>
2016-06-06 11:34:33 +1000

    Fix deprecated unicode codepoint syntax

--- test/graphql/lang/lexer_test.exs
+++ test/graphql/lang/lexer_test.exs
@@ -12,18 +12,18 @@ defmodule GraphQL.Lang.Lexer.LexerTest do
 
   # Ignored tokens
   test "WhiteSpace is ignored" do
-    assert_tokens '\x{0009}', [] # horizontal tab
-    assert_tokens '\x{000B}', [] # vertical tab
-    assert_tokens '\x{000C}', [] # form feed
-    assert_tokens '\x{0020}', [] # space
-    assert_tokens '\x{00A0}', [] # non-breaking space
+    assert_tokens '\u0009', [] # horizontal tab
+    assert_tokens '\u000B', [] # vertical tab
+    assert_tokens '\u000C', [] # form feed
+    assert_tokens '\u0020', [] # space
+    assert_tokens '\u00A0', [] # non-breaking space
   end
 
   test "LineTerminator is ignored" do
-    assert_tokens '\x{000A}', [] # new line
-    assert_tokens '\x{000D}', [] # carriage return
-    assert_tokens '\x{2028}', [] # line separator
-    assert_tokens '\x{2029}', [] # paragraph separator
+    assert_tokens '\u000A', [] # new line
+    assert_tokens '\u000D', [] # carriage return
+    assert_tokens '\u2028', [] # line separator
+    assert_tokens '\u2029', [] # paragraph separator
   end
 
   test "Comment is ignored" do
