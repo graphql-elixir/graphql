15008c8d1b572d122d0e4c6fef7934eab52fbe5d
James Sadler <freshtonic@gmail.com>
2016-07-09 16:26:51 +0930

    Extract an `expand_options` function

--- lib/graphql/execution/executor.ex
+++ lib/graphql/execution/executor.ex
@@ -24,14 +24,18 @@ defmodule GraphQL.Execution.Executor do
   @spec execute(GraphQL.Schema.t, GraphQL.Document.t, list) :: result_data | {:error, %{errors: list}}
   def execute(schema, document, opts \\ []) do
     schema          = Schema.with_type_cache(schema)
-    root_value      = Keyword.get(opts, :root_value, %{})
-    variable_values = Keyword.get(opts, :variable_values, %{})
-    operation_name  = Keyword.get(opts, :operation_name, nil)
+    {root_value, variable_values, operation_name} = expand_options(opts)
     context = ExecutionContext.new(schema, document, root_value, variable_values, operation_name)
     case context.errors do
       [] -> execute_operation(context, context.operation, root_value)
       _  -> {:error, %{errors: Enum.dedup(context.errors)}}
     end
+  end
+
+  defp expand_options(opts) do
+    {Keyword.get(opts, :root_value, %{}),
+     Keyword.get(opts, :variable_values, %{}),
+     Keyword.get(opts, :operation_name, nil)}
   end
 
   @spec execute_operation(ExecutionContext.t, Nodes.operation_node, map) :: result_data | {:error, String.t}
